195e45eeae01bcb64c027e7a2d2c59aacc5a462b
Austin Amani <amaniaustin123@gmail.com>
2024-04-21 20:34:30 +0300

    Started working on the Update part of CRUD

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { TiEdit } from 'react-icons/ti'
+
 
 const ToDoItem = ({toDoItem}) => {
   return (
     <>
-    <div className="border-b-2 border-white p-4 ">
-      <h1>{toDoItem.title}</h1>
-      <p>{toDoItem.time}</p>
+    <div className="border-b-2 border-white p-4 flex justify-between">
+      <div>
+        <h1>{toDoItem.title}</h1>
+        <p>{toDoItem.time}</p>
+      </div>
+      <div className='mt-4'>
+        <TiEdit />
+      </div>
     </div>
     </>
   )
